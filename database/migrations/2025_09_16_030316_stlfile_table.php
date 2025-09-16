37295e811b09848a7aaa6055c169ee949cad0354
Daniel <dtorresc@it.ucsc.cl>
2025-09-16 20:22:35 -0300

    primer commit

---
+++ database/migrations/2025_09_16_030316_stlfile_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stlfile', function (Blueprint $table) {
+            $table->id(); // This creates an unsignedBigInteger
+            $table->string('fileName');
+            $table->string('filePath');
+            $table->unsignedBigInteger('category_id');
+            $table->boolean('isActive')->default(true);
+            $table->timestamps();
+             $table->foreign('category_id')->references('id')->on('category');
+        });
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('stlfile');
+    }
+};
---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>categorias</title>
+</head>
+<body>
+    wena wena llegamos hasta aca.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit in exercitationem debitis saepe quia! Neque, ipsa sunt ab illo eos impedit, dolorum exercitationem tempora doloremque libero adipisci accusamus soluta fugit!
+</body>
+</html>
